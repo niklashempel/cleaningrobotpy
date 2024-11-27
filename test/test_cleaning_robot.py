f0b5f3e29e97643744251325391ca295741f2a54
Niklas Hempel <niklashempel9@gmail.com>
2024-11-27 21:35:40 +0100

    #1 Red phase test_robot_status

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -14,3 +14,11 @@ class TestCleaningRobot(TestCase):
         self.assertEqual(0, sut.pos_x)
         self.assertEqual(0, sut.pos_y)
         self.assertEqual(sut.N, sut.heading)
+
+    def test_robot_status(self):
+        sut = CleaningRobot()
+        sut.pos_x = 1
+        sut.pos_y = 3
+        sut.heading = sut.W
+        status = sut.robot_status()
+        self.assertEqual("(1,3,W)", status)
