0b7f3b88ee56c7148d8119b1903e42a5d23b6886
Niklas Hempel <niklashempel9@gmail.com>
2024-11-28 21:05:20 +0100

    #2 Red phase

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -22,3 +22,28 @@ class TestCleaningRobot(TestCase):
         sut.heading = sut.W
         status = sut.robot_status()
         self.assertEqual("(1,3,W)", status)
+
+    @patch.object(IBS, 'get_charge_left')
+    @patch.object(GPIO, 'output')
+    def test_manage_cleaning_system_charge_above_10(self, mock_gpio: Mock, mock_get_charge_left: Mock):
+        mock_get_charge_left.return_value = 11
+        sut = CleaningRobot()
+        sut.manage_cleaning_system()
+        self.assertTrue(sut.cleaning_system_on)
+        self.assertFalse(sut.recharge_led_on)
+        output_calls =[call(sut.CLEANING_SYSTEM_PIN, True), call(sut.RECHARGE_LED_PIN, False)]
+        mock_gpio.assert_has_calls(output_calls)
+
+    @patch.object(IBS, 'get_charge_left')
+    @patch.object(GPIO, 'output')
+    def test_manage_cleaning_system_charge_less_or_equal_10(self, mock_gpio: Mock, mock_get_charge_left: Mock):
+        mock_get_charge_left.return_value = 10
+        sut = CleaningRobot()
+        sut.manage_cleaning_system()
+        self.assertFalse(sut.cleaning_system_on)
+        self.assertTrue(sut.recharge_led_on)
+        output_calls =[call(sut.CLEANING_SYSTEM_PIN, False), call(sut.RECHARGE_LED_PIN, True)]
+        mock_gpio.assert_has_calls(output_calls)
+
+    
+
